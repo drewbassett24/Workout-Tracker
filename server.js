32f02f664b31fdbf4139f27e776284ce13dd0519
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-04-27 18:41:55 +0100

    changed default PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3003
 
 const app = express();
 
