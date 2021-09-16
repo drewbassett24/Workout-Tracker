6f2993eec66acacbc97a0f4870b9a2ba2d5170ec
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-09-16 19:09:59 +0100

    Correctedmongodb connection

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(routes);
 
-
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workoutAtlas',
-    {useNewUrlParser: true});
+    //process.env.MONGODB_URI || 'mongodb://localhost/WorkoutTracker',
+    "mongodb+srv://DB:Bastenfyr3@trainer-cluster.2poqm.mongodb.net/workoutTracker?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true
+    });
 
 app.listen(PORT, () => {
-    console.log('App running on port ${PORT}.');
+    console.log(`App running on port ${PORT}.`);
 });
