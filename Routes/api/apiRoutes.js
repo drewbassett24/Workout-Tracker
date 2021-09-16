7ad22bb33bc51725746cd38bcb43e04f8ced2868
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-09-16 19:11:40 +0100

    Switched try/catch to then

--- Routes/api/apiRoutes.js
+++ Routes/api/apiRoutes.js
@@ -1,12 +1,11 @@
 const Workout = require('../../models/workoutModel');
-const mongojs = require('mongojs');
-const express = require('express');
+// const express = require('express');
 const router = require('express').Router();
 
 // POST route
 // Creates a new workout model & adds it to the DB
 router.post('/api/workouts', (req, res) => {
-    Workout.create({}).try(
+    Workout.create({}).then(
         (fitnessdb) => {
             res.json(fitnessdb);
         }
@@ -19,7 +18,15 @@ router.post('/api/workouts', (req, res) => {
 
 // GET routes
 router.get('/api/workouts', (req, res) => {
-    Workout.find({}).try(
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration',
+                },
+            },
+        },
+    ]).then(
         (fitnessdb) => {
             res.json(fitnessdb)
         }
@@ -46,9 +53,9 @@ router.get('/api/workouts/range', (req, res) => {
 router.put('/api/workouts/:id', (req, res) => {
     const id = mongojs.ObjectId(req.params.id);
     Workout.findOneAndUpdate(
-        {_id: id},
-        {$push: {exercises: body}},
-        {new: true}
+        { _id: id },
+        { $push: { exercises: body } },
+        { new: true }
     ).try(
         (fitnessdb) => {
             res.json(fitnessdb);
